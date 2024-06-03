a3c9e90b5ba4a98e440f84fcd00f9420ea53bf82
Cristel <stelda@outlook.fr>
2024-06-03 16:27:21 +0200

    Style sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import yoga from '../assets/sidebar_yoga.png';
+import swimmer from '../assets/sidebar_swimmer.png';
+import bicycle from '../assets/sidebar_bicycle.png';
+import dumbbell from '../assets/sidebar_dumbbell.png';
 
 function Sidebar() {
     return (
         <aside className="sidebar">
             <ul>
-                <li><a href="#"><img src="/assets/sidebar_yoga.png" alt="Yoga"/></a></li>
-                <li><a href="#"><img src="../assets/sidebar_swimmer.png" alt="Swimmer"/></a></li>
-                <li><a href="#"><img src="../assets/sidebar_bicycle.png" alt="Bicycle"/></a></li>
-                <li><a href="#"><img src="../assets/sidebar_dumbbell.png" alt="Dumbbell"/></a></li>
+                <li><a href="#"><img src={yoga} alt="Yoga"/></a></li>
+                <li><a href="#"><img src={swimmer} alt="Swimmer"/></a></li>
+                <li><a href="#"><img src={bicycle} alt="Bicycle"/></a></li>
+                <li><a href="#"><img src={dumbbell} alt="Dumbbell"/></a></li>
             </ul>
             <p>Copyright, SportSee 2024</p>
         </aside>
