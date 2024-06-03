abcca50c716db098db1f77d1e793dec3810f77d7
Cristel <stelda@outlook.fr>
2024-06-03 11:30:23 +0200

    Style nutrition stats

--- src/components/NutritionStats.js
+++ src/components/NutritionStats.js
@@ -1,23 +1,47 @@
 import React from 'react';
+import iconCalories from '../assets/icon_calories.png';
+import iconProteins from '../assets/icon_proteins.png';
+import iconCarbs from '../assets/icon_carbs.png';
+import iconFats from '../assets/icon_fats.png';
 
 function NutritionStats() {
     return (
         <div className="nutrition-stats">
             <div className="nutrition-calories">
-                <img src="icons/burned-calories.svg" alt="burned calories" />
-                <p>725 <span>Calories</span></p>
+                <div className="icon-background">
+                    <img src={iconCalories} alt="calories icon" />
+                </div>
+                <div className="nutrition-informations">
+                    <p>725<span>kCal</span></p>
+                    <p>Calories</p>
+                </div>
             </div>
-            <div className="nutrition-protein">
-                <img src="icons/protein.svg" alt="protein" />
-                <p>25 <span>g</span></p>
+            <div className="nutrition-proteins">
+                <div className="icon-background">
+                    <img src={iconProteins} alt="proteins icon" />
+                </div>
+                <div className="nutrition-informations">
+                    <p>25<span>g</span></p>
+                    <p>Protéines</p>
+                </div>
             </div>
             <div className="nutrition-carbs">
-                <img src="icons/carbs.svg" alt="carbs" />
-                <p>50 <span>g</span></p>
+                <div className="icon-background">
+                    <img src={iconCarbs} alt="carbs icon" />
+                </div>
+                <div className="nutrition-informations">
+                    <p>50<span>g</span></p>
+                    <p>Glucides</p>
+                </div>
             </div>
-            <div className="nutrition-fat">
-                <img src="icons/fat.svg" alt="fat" />
-                <p>15 <span>g</span></p>
+            <div className="nutrition-fats">
+                <div className="icon-background">
+                    <img src={iconFats} alt="fats icon" />
+                </div>
+                <div className="nutrition-informations">
+                    <p>15<span>g</span></p>
+                    <p>Lipides</p>
+                </div>
             </div>
         </div>
     );
